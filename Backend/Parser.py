10d7f0d24906fa41919ba50b248b92b3ff3ca7c3
Aditya Lallan Gupta <97335636+AdityaGupta32@users.noreply.github.com>
2026-01-27 07:59:03 +0530

    Update Parser.py

--- Backend/Parser.py
+++ Backend/Parser.py
@@ -19,7 +19,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                    https://fin-flow-mauve.vercel.app/],
+                   "https://fin-flow-mauve.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
