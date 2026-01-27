ab757185cf9d34ad7c848f8d8e4d72d1ab2c5808
Aditya Lallan Gupta <97335636+AdityaGupta32@users.noreply.github.com>
2026-01-27 07:59:37 +0530

    Fix CORS configuration for frontend

--- Backend/main.py
+++ Backend/main.py
@@ -11,7 +11,7 @@ app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["http://localhost:5173",
-                    https://fin-flow-mauve.vercel.app/],
+                   "https://fin-flow-mauve.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
