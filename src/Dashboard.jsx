94d639576a6a8a906d464093f465c7d7f4ad1acf
Aditya Lallan Gupta <97335636+AdityaGupta32@users.noreply.github.com>
2026-01-27 07:35:40 +0530

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -92,7 +92,7 @@ const Dashboard = ({ transactions = [], predictionData = null, user = null, onUp
     formData.append("credit_score", profile.credit_score);
 
     try {
-      const response = await fetch("http://localhost:8000/predict", {
+      const response = await fetch("https://fintech-backend-gufe.onrender.com/predict", {
         method: "POST",
         body: formData, 
       });
